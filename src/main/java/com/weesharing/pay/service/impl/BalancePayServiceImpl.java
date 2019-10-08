8ad7c891fc7738eb89f17fd6ad6b3c9c5c409e33
ZhangPeng <peng.zhang@weesharing.com>
2019-10-08 13:49:42 +0800

    打印余额退款日志

--- src/main/java/com/weesharing/pay/service/impl/BalancePayServiceImpl.java
+++ src/main/java/com/weesharing/pay/service/impl/BalancePayServiceImpl.java
@@ -56,7 +56,7 @@ public class BalancePayServiceImpl implements WSPayService{
 	public void doRefund(Refund refund) {
 		BalanceRefundData  trd = new BalanceRefundData(refund);
 		CommonResult2<RefundResult> commonResult = BeanContext.getBean(SSOService.class).refund(trd);
-		log.debug("请求余额退款参数: {}, 结果: {}", JSONUtil.wrap(trd, false), JSONUtil.wrap(commonResult, false));
+		log.info("请求余额退款参数: {}, 结果: {}", JSONUtil.wrap(trd, false), JSONUtil.wrap(commonResult, false));
 		if (commonResult.getCode() == 200) {
 			refund.setTradeDate(DateUtil.format(new Date(), "yyyyMMddHHmmss"));
 			refund.setStatus(1);
